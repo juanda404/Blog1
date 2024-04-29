a690a9ab5169a1233b9594c72eef61d7fbb2f488
Juan David Santamaria <juanda404@gmail.com>
2024-04-29 18:29:19 -0500

    se logra configurar el archivo css en el proyecto

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,4 +1,18 @@
 @extends('template')
 @section('content')
-    <h1>aqui iniciamos el post</h1>
+    <h1>Create Post</h1>
+    <form action="" method="post">
+        <div>
+            <label>Title</label>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <textarea name="" id="" cols="60" rows="20"></textarea>
+            <input type="text" name="" id="" placeholder="Categories">
+        </div>
+        <div>
+            <label>Publish now?</label>
+            <input type="button" value="Send">
+        </div>
+    </form>
 @endsection 
